e607526abf0eef37b81cf53c715faa4c1ca7e2f7
Ubuntu <ubuntu@ip-172-31-15-17.us-east-2.compute.internal>
2021-05-03 20:01:30 +0000

    first attempt at rspec setup

---
+++ spec/controllers/job_year_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe JobYearController do
+    describe "Jobs for job year correctly shown" do
+        it "Dummy Test" do
+            puts "Howdy!"
+        end
+    end
+end
